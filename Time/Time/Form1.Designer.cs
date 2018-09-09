90c53d6fc47385a54066d7d3b6e0af20b034e6ac
Dake <34675463+Limon2033@users.noreply.github.com>
2018-09-09 20:55:11 +0600

    Add files via upload

---
+++ Time/Time/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Time
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.msec = new System.Windows.Forms.Label();
+            this.sec = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(649, 360);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(86, 360);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Старт";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(198, 360);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(75, 23);
+            this.Pause.TabIndex = 2;
+            this.Pause.Text = "Пауза";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(316, 360);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "Стоп";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(593, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Время";
+            // 
+            // msec
+            // 
+            this.msec.AutoSize = true;
+            this.msec.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.msec.Location = new System.Drawing.Point(436, 96);
+            this.msec.Name = "msec";
+            this.msec.Size = new System.Drawing.Size(127, 91);
+            this.msec.TabIndex = 5;
+            this.msec.Text = "00";
+            // 
+            // sec
+            // 
+            this.sec.AutoSize = true;
+            this.sec.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sec.Location = new System.Drawing.Point(236, 96);
+            this.sec.Name = "sec";
+            this.sec.Size = new System.Drawing.Size(127, 91);
+            this.sec.TabIndex = 6;
+            this.sec.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(369, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 91);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(259, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 46);
+            this.label3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sec);
+            this.Controls.Add(this.msec);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Таймер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label msec;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label sec;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Time/Time/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Time
+{
+    public partial class Form1 : Form
+    {
+        int timemsec, timesec;
+        bool stop = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timesec == 0 && timemsec == 0)
+            {
+                timer1.Stop();
+                label3.Text = "Время вышло";
+                Activate();
+                Reset();
+                textBox1.Enabled = true;
+            }
+            else
+            {
+                if (timemsec == 0)
+                {
+                    DrawTime();
+                    timesec--;
+                    timemsec = 99;
+                }
+                else
+                {
+                    DrawTime();
+                    timemsec--;
+                }
+            }
+        }
+
+        private void DrawTime()
+        {
+            sec.Text = String.Format("{0:00}", timesec);
+            msec.Text = String.Format("{0:00}", timemsec);
+        }
+
+        private void Reset()
+        {
+            sec.Text = "00";
+            msec.Text = "00";          
+            textBox1.Text = "";
+            textBox1.Enabled = true;
+            stop = false;
+        }
+
+        private void Activate()
+        {
+            Pause.Enabled = false;
+            Stop.Enabled = false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Activate();
+            textBox1.MaxLength = 2;
+            Start.Enabled = false;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (stop)
+            {
+                timer1.Enabled = true;
+                Start.Text = "Старт";
+            }
+            else
+            {
+                timesec = Int32.Parse(textBox1.Text);
+                timemsec = 0;
+                textBox1.Enabled = false;
+                timer1.Start();
+            }
+            Start.Enabled = false;
+            Pause.Enabled = true;
+            Stop.Enabled = true;
+            label3.Text = "";
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Start.Enabled = true;
+            Pause.Enabled = false;
+            stop = true;
+            Start.Text = "Продолжить";
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Activate();
+            Start.Text = "Старт";
+            msec.Text = "";
+            Reset();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0)
+            {
+                Start.Enabled = true;
+            }
+            else
+            {
+                Start.Enabled = false;
+            }
+        }
+    }
+}
